bf87949cf4c094b51914bfc1ec054d508496a5f0
Sergio Pérez <sergi9p@gmail.com>
2018-12-14 12:19:05 +0100

    Email log change

--- authentication/controllers/emailConfig.js
+++ authentication/controllers/emailConfig.js
@@ -26,8 +26,6 @@ const sendEmail = (user, token) => {
     if (error) {
       return console.log(error);
     }
-    console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   });
 };
 
